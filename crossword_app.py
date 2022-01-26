e1ff94946692d337aa29a254ffa17522eb7545d9
jte2004 <jte2004@nyu.edu>
2022-01-26 10:42:12 -0500

    Update crossword_app.py

--- crossword_app.py
+++ crossword_app.py
@@ -68,7 +68,7 @@ st.header('Trends Over Time')
 day = st.selectbox('Pick Weekday',days)
 
 fig, ax = plt.subplots()
-ax.set_title(day+' Trends')
+fig.patch.set_alpha(0.1)
 ax.plot(df.Week, df_running_avg[day], label='Running Average')
 ax.plot(df.Week, df_20[day], label='L20 Average')
 ax.plot(df.Week, df_52[day], label='L52 Average')
